937dfe1dedf980d4a143a739341e01adef5cc28f
Haardik Garg <thehaardikgarg@gmail.com>
2025-07-21 09:49:48 -0700

    use selenium stealth to bypass bot checks

--- src/scraper.py
+++ src/scraper.py
@@ -2,46 +2,53 @@
 from bs4 import BeautifulSoup
 from proxy import startProxy, stopProxy
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
 import time
 
 
 PROXIES = {
-    'http': 'socks5://127.0.0.1:9050',
-    'https': 'socks5://127.0.0.1:9050'
+    'https': 'socks5://127.0.0.1:9050',
+    'http': 'socks5://127.0.0.1:9050'
 }
-
 
 chrome_options = Options()
 chrome_options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
-# chrome_options.add_argument("--headless")
-service = Service(executable_path="./chromedriver.exe")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=chrome_options)
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win64",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 
 
 def scrape(searches):
-    try:
-        process = startProxy(PROXIES)
-    except:
-        stopProxy(process)
-        process = startProxy(PROXIES)
+    process = startProxy(PROXIES)
+    
     text = searches[0]
-    url = 'https://youtube.com/'
-    page = driver.get(url)
+    url = 'https://www.startpage.com/'
+    driver.get(url)
+    print('went to page')
+    time.sleep(3)
+
     time.sleep(2)
-    search_box = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.NAME, 'search_query'))
-    )
-    search_box.clear()
+
+    search_box = driver.find_element(By.ID, 'q')
+    print('found serach box')
+
     search_box.send_keys(text)
     search_box.send_keys(Keys.RETURN)
-    soup = BeautifulSoup(page.text, "html.parser")
-    link = soup.find_all('a')
-    print(link)
+
     time.sleep(10)  
-    driver.close()
+    driver.quit()
