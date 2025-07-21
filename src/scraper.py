ec7419357a0706c522174c18a42803ad80361e6a
Haardik Garg <thehaardikgarg@gmail.com>
2025-07-21 12:55:26 -0700

    scrape links from browser search

--- src/scraper.py
+++ src/scraper.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from proxy import startProxy, stopProxy
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -21,34 +22,50 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=chrome_options)
+service = Service(executable_path='./chromedriver.exe')
 
-stealth(driver,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win64",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        )
 
 
 def scrape(searches):
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win64",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+    
     process = startProxy(PROXIES)
     
     text = searches[0]
     url = 'https://www.startpage.com/'
     driver.get(url)
     print('went to page')
-    time.sleep(3)
 
-    time.sleep(2)
+    time.sleep(5)
 
     search_box = driver.find_element(By.ID, 'q')
     print('found serach box')
 
     search_box.send_keys(text)
     search_box.send_keys(Keys.RETURN)
+    print('searched query')
+    page = driver.page_source
 
-    time.sleep(10)  
+    time.sleep(3)
+
+    soup = BeautifulSoup(page, 'html.parser')
+    a_elements = soup.find_all('a', class_='result-title result-link css-1ubyvt6')
+    hrefs = []
+    for a in a_elements:
+        hrefs.append(a.get('href'))
+    
+    print(hrefs)
+    
+
+    time.sleep(5)
     driver.quit()
+
